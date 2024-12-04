c837a28212e454e37acd6bda53c86e5d2298fdaf
Komp <artemshoshura@mail.ru>
2024-12-04 17:07:29 +0300

    правки после проверки

--- src/vacancy.py
+++ src/vacancy.py
@@ -2,19 +2,20 @@ class Vacancy:
     __slots__ = ["title", "company", "salary", "link"]
 
     def __init__(self, title: str, company: str, salary: int, link: str):
-        self.title = self._validate_string(title)
-        self.company = self._validate_string(company)
-        self.salary = self._validate_salary(salary)
-        self.link = self._validate_string(link)
+        self.title = self._non_empty_string_validator(title)
+        self.company = self._non_empty_string_validator(company)
+        self.salary = self._non_empty_string_validator(salary)
+        self.link = self._non_empty_string_validator(link)
 
-    def _validate_string(self, value: str) -> str:
+    def _non_empty_string_validator(self, value: str) -> str:
         if not value:
             raise ValueError("Строковое значение не может быть пустым")
         return value
 
-    def _validate_salary(self, value: int) -> int:
+    def validate_salary(value: int) -> int:
+        """Проверяет, что зарплата неотрицательная."""
         if value < 0:
-            return 0
+            raise ValueError("Зарплата не может быть отрицательной")
         return value
 
     def __repr__(self):
